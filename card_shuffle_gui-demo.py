dcf34f87e4a8edc4aab7aed090333a2f9595128a
Barry <bharrisaplus@gmail.com>
2025-12-24 14:30:30 -0500

    Add tkinter demo

--- card_shuffle_gui-demo.py
+++ card_shuffle_gui-demo.py
@@ -1,9 +1,10 @@
 import argparse
 import turtle
+import tkinter
 
 import card_shuffle_constants
 
-def turtle_decklist():
+def turtle_demo():
     ace_spade = chr(int(card_shuffle_constants.card_to_utf8.get(('spade', 1)), 16))
     ace_diamond = chr(int(card_shuffle_constants.card_to_utf8.get(('diamond', 1)), 16))
 
@@ -21,6 +22,20 @@ def turtle_decklist():
 
     turtle.mainloop()
 
+def tkinter_demo():
+    ace_spade = chr(int(card_shuffle_constants.card_to_utf8.get(('spade', 1)), 16))
+    two_spade = chr(int(card_shuffle_constants.card_to_utf8.get(('spade', 2)), 16))
+
+    cardFontStyle = ('Consolas', 81)
+
+    tkinterWindow = tkinter.Tk()
+
+    tkinterWindow.title("pcs: pseudo card shuffle")
+    tkinterWindow.geometry("1036x583")
+
+    tkinterWindow.mainloop()
+
 if __name__ == "__main__":
 
-    turtle_decklist()
+    #turtle_demo()
+    tkinter_demo()

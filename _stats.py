8b49d73cfe3ea8f804b25382903d672c898247ff
Barry <bharrisaplus@gmail.com>
2025-12-27 21:25:16 -0500

    Cleanup jaro method

--- _stats.py
+++ _stats.py
@@ -1,23 +1,23 @@
-def count_jaro_match(x, y, z):
-    _matched = []
+def get_jaro_match_between(mixed_order, known_order):
+    tolerance = ( max(len(mixed_order), len(known_order)) // 2 ) - 1
+    _matched_x = [0] * len(mixed_order)
+    _matched_y = [0] * len(known_order)
 
-    for idx, itm in enumerate(x):
-        l_bound = max(idx - z, 0)
-        u_bound = min(idx + z, len(y))
+    for idx_x, itm_x in enumerate(mixed_order):
+        l_bound = max(idx_x - tolerance, 0)
+        u_bound = min(idx_x + tolerance, len(known_order))
 
-        if itm in y[l_bound:u_bound] and itm not in _matched:
-            _matched.append(itm)
+        for idx_y, itm_y in enumerate(known_order[l_bound:u_bound], start=l_bound):
+            if itm_x == itm_y and itm_y not in _matched_y:
+                _matched_x.insert(idx_x, itm_x)
+                _matched_y.insert(idx_y, itm_y)
 
-    return _matched
+    return (
+        [maybe_itm for maybe_itm in _matched_x if maybe_itm != 0],
+        [maybe_itm for maybe_itm in _matched_y if maybe_itm != 0]
+    )
 
-def get_jaro_match(mixed_order, known_order):
-    seq_tol = ( max(len(mixed_order), len(known_order)) // 2 ) - 1
-    mixed_matches = count_jaro_match(mixed_order, known_order, seq_tol)
-    known_matches = count_jaro_match(known_order, mixed_order, seq_tol)
-
-    return mixed_matches, known_matches
-
-def count_jaro_transposition(mixed_ref, known_ref):
+def count_jaro_transposition_for(mixed_ref, known_ref):
     t_count = 0
 
     for idx, itm in enumerate(mixed_ref):
@@ -26,9 +26,9 @@ def count_jaro_transposition(mixed_ref, known_ref):
 
     return t_count / 2
 
-def get_jaro_similarity(mixed_order, known_order):
-    mixed_match, known_match = get_jaro_match(mixed_order, known_order)
-    transposition_count = count_jaro_transposition(mixed_match, known_match)
+def get_jaro_similarity_from(mixed_order, known_order):
+    mixed_match, known_match = get_jaro_match_between(mixed_order, known_order)
+    transposition_count = count_jaro_transposition_for(mixed_match, known_match)
 
     match_count = len(mixed_match)
 

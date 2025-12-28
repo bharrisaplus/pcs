c170fea7dfd00599f9746041a864ea4df891bf6b
Barry <bharrisaplus@gmail.com>
2025-12-28 14:55:15 -0500

    Add docstrings for stat methods

--- _stats.py
+++ _stats.py
@@ -1,4 +1,18 @@
 def get_jaro_match_between(mixed_order, known_order):
+    '''Gather a characters from each list that are aligned with some deviation allowed
+
+    The deviation being, with len(mixed_order) -> a, len(known_order) -> b:
+
+            ⌊ max(a, b) * 0.5 ⌋ - 1
+
+    Args:
+        mixed_order (list[str]): The randomized items
+        known_order (list[str]): The sorted items
+
+    Returns:
+        tuple(list[str], list[str]):
+    '''
+
     tolerance = ( max(len(mixed_order), len(known_order)) // 2 ) - 1
     _matched_x = [0] * len(mixed_order)
     _matched_y = [0] * len(known_order)
@@ -17,16 +31,41 @@ def get_jaro_match_between(mixed_order, known_order):
         [maybe_itm for maybe_itm in _matched_y if maybe_itm != 0]
     )
 
-def count_jaro_transposition_for(mixed_ref, known_ref):
+def count_jaro_transposition_for(mixed_order, known_order):
+    '''How many things need to be rearranged so that the lists match.
+    
+    After finding the matched characters, compare their current positions between the two lists
+
+    Args:
+        mixed_order (list[str]): The randomized items
+        known_order (list[str]): The sorted items
+
+    Returns:
+        float:
+    '''
+
     t_count = 0
 
-    for idx, itm in enumerate(mixed_ref):
-        if itm is not known_ref[idx]:
+    for idx, itm in enumerate(mixed_order):
+        if itm is not known_order[idx]:
             t_count += 1
 
     return t_count / 2
 
 def get_jaro_edit_distance_from(mixed_order, known_order):
+    '''An implementation of the jaro distance metric.
+
+    Helpful check in determining how varied/randomized a shuffled deck of cards is given their
+        original positions.
+
+    Args:
+        mixed_order (list[str]): The randomized items
+        known_order (list[str]): The sorted items
+
+    Returns:
+        tuple(float, tuple(list[str], list[str]), int):
+    '''
+
     mixed_match, known_match = get_jaro_match_between(mixed_order, known_order)
     transposition_count = count_jaro_transposition_for(mixed_match, known_match)
 
@@ -39,6 +78,21 @@ def get_jaro_edit_distance_from(mixed_order, known_order):
     return (1/3) * ( a + b + c ), (mixed_match, known_match), transposition_count
 
 def count_peapods_from(mixed_order, known_order):
+    '''Finding pairs of consecutive cards.
+
+    Another helpful check in determining variance for cards compared to their original positions.
+        When going over the cards each group of two are peapods - either ripe and green.
+            * Ripe is in order.
+            * Green is random
+
+    Args:
+        mixed_order (list[str]): The randomized items
+        known_order (list[str]): The sorted items
+
+    Returns:
+        tuple(int, int):
+    '''
+
     item_count = len(mixed_order)
     _ripe_counter = 0
     _green_counter = 0

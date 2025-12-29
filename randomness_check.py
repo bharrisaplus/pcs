98f31a738665e187e0912c596da577f64f923050
Barry <bharrisaplus@gmail.com>
2025-12-29 12:07:15 -0500

    Remove tests for _stat from randomness_check

--- randomness_check.py
+++ randomness_check.py
@@ -1,101 +1,16 @@
 import unittest
 
-import _stats as staty
+import _stats as CardShuffleStats
+import card_shuffle as CardShuffle
 
-card_order = [('spade', 1),('spade', 2),('spade', 3),('spade', 4),('spade', 5),('spade', 6),('spade', 7),('spade', 8),('spade', 9),('spade', 10),('spade', 11),('spade', 12),('spade', 13),('diamond', 1),('diamond', 2),('diamond', 3),('diamond', 4),('diamond', 5),('diamond', 6),('diamond', 7),('diamond', 8),('diamond', 9),('diamond', 10),('diamond', 11),('diamond', 12),('diamond', 13),('club', 13),('club', 12),('club', 11),('club', 10),('club', 9),('club', 8),('club', 7),('club', 6),('club', 5),('club', 4),('club', 3),('club', 2),('club', 1),('heart', 13),('heart', 12),('heart', 11),('heart', 10),('heart', 9),('heart', 8),('heart', 7),('heart', 6),('heart', 5),('heart', 4),('heart', 3),('heart', 2),('heart', 1)]
+from specs import card_order
 
 class RandomCheck(unittest.TestCase):
-    def test_jaro(self):
-        ex_a = 'FARMVILLE'
-        ex_b = 'FAREMVIEL'
-        solution = staty.get_jaro_edit_distance_from(ex_b, ex_a)
+    def test_card_shuffle(self):
+        mixed_up = CardShuffle.shuffle_cards(card_order, list(range(len(card_order))))
 
-        self.assertEqual(0.8842592592592592, solution[0],
-            "jaro similarity between {} and {} should be 0.8842592592592592".format(ex_a, ex_b)
+        self.assertEqual(len(mixed_up), len(card_order),
+            "The shuffled deck should retain the same number of cards as before the shuffle"
         )
 
-        self.assertEqual(len(solution[1][0]), 8,
-            "matched characters in {} and {} should be 8".format(ex_a, ex_b)
-        )
-
-        self.assertEqual(len(solution[1][1]), 8,
-            "matched characters in {} and {} should be 8".format(ex_a, ex_b)
-        )
-
-        self.assertEqual(solution[2], 2/2,
-            "transpositions counted in {} and {} should be 1".format(ex_a, ex_b)
-        )
-
-    def test_jaro_again(self):
-        ex_c = 'HELLO'
-        ex_d = 'HEYYA'
-        solution = staty.get_jaro_edit_distance_from(ex_c, ex_d)
-
-        self.assertEqual(1.8/3, solution[0],
-            "jaro similarity between {} and {} should be 0.6".format(ex_c, ex_d)
-        )
-
-        self.assertEqual(len(solution[1][0]), 2,
-            "matched characters in {} and {} should be 2".format(ex_c, ex_d)
-        )
-
-        self.assertEqual(len(solution[1][1]), 2,
-            "matched characters in {} and {} should be 2".format(ex_c, ex_d)
-        )
-
-        self.assertEqual(solution[2], 0,
-            "transpositions counted in {} and {} should be 0".format(ex_c, ex_d)
-        )
-
-    def test_jaro_again_again(self):
-        ex_e = 'XLNGXATCXR'
-        ex_f = 'FYJLHDRQDM'
-        solution = staty.get_jaro_edit_distance_from(ex_e, ex_f)
-
-        self.assertEqual(abs(1.4/3), solution[0],
-            "jaro edit distance between {} and {} should be ~0.4667".format(ex_e, ex_f)
-        )
-
-        self.assertEqual(len(solution[1][0]), 2,
-            "matched characters in {} and {} should be ".format(ex_e, ex_f)
-        )
-
-        self.assertEqual(len(solution[1][1]), 2,
-            "matched characters in {} and {} should be ".format(ex_e, ex_f)
-        )
-
-        self.assertEqual(solution[2], 0,
-            "transpositions counted in {} and {} should be ".format(ex_e, ex_f)
-        )
-
-    def test_peapod(self):
-        ex_g = [16,18,13,15,11,12,14,10,17]
-        ex_h = [10,11,12,13,14,15,16,17,18]
-
-        solution = staty.count_peapods_from(ex_g, ex_h)
-
-        self.assertEqual(solution[0], 1, "Should be 1 ripe peapod in {}".format(ex_g))
-        self.assertEqual(solution[1], 15, "Should be 15 green peapods in {}".format(ex_h))
-
-    def test_peapod_agin(self):
-        ex_i = [24,25,19,18,23,21,22,26,20]
-        ex_j = [18,19,20,21,22,23,24,25,26]
-
-        solution = staty.count_peapods_from(ex_i, ex_j)
-
-        self.assertEqual(solution[0], 3, "Should be 3 ripe peapods in {}".format(ex_i))
-        self.assertEqual(solution[1], 13, "Should be 13 green peapods in {}".format(ex_j))
-
-    def test_peapod_again_again(self):
-        ex_k = [6,2,4,1,7,5,8,3]
-        ex_l = [1,2,3,4,5,6,7,8]
-
-        solution = staty.count_peapods_from(ex_k, ex_l)
-
-        self.assertEqual(solution[0], 0, "Should be 0 ripe peapods in {}".format(ex_k))
-        self.assertEqual(solution[1], 14, "Should be 13 green peapods in {}".format(ex_l))
-
-
-    #def test_card_shuffle(self):
-
-    #def test_card_shuffle_more(self):
+    #def test_card_shuffle_long(self):

cf566ad75fdb56be1a9e82568b18487ddd3b6255
Barry <bharrisaplus@gmail.com>
2025-12-30 20:24:09 -0500

    Add more checks for consecutive pairs and move common variable to setup method

--- randomness_check.py
+++ randomness_check.py
@@ -17,12 +17,15 @@ class RandomCheck(unittest.TestCase):
             | GSR     | 0.6662 | 0.0363 | 0.5252 |  0.8017 |
             +---------+--------+--------+--------+---------+
 
-    Next looking at 'peapods' or pairs of cards that are consecutive matching their positions in
+    Next looking at 'peapods' or pairs of cards that are consecutive or matching their positions in
         new deck order.
     '''
+
+    def setUp(self):
+        self.new_deck_order_positions = list(range(len(card_order)))
+
     def test_card_shuffle(self):
-        card_order_len = len(card_order)
-        mixed_up = CardShuffle.shuffle_cards(card_order, list(range(card_order_len)))
+        mixed_up = CardShuffle.shuffle_cards(card_order, self.new_deck_order_positions)
 
         jaro_similarity = CardShuffleStats.get_jaro_edit_distance_from(mixed_up, card_order)
 
@@ -31,8 +34,7 @@ class RandomCheck(unittest.TestCase):
         )
 
     def test_card_shuffle_peapod_cut(self):
-        card_order_len = len(card_order)
-        mixed_up = CardShuffle.shuffle_cards(card_order, list(range(card_order_len)))
+        mixed_up = CardShuffle.shuffle_cards(card_order, self.new_deck_order_positions)
         maybe_cut_up, _ = CardShuffle.maybe_cut(mixed_up)
 
         jaro_similarity = CardShuffleStats.get_jaro_edit_distance_from(maybe_cut_up, card_order)
@@ -42,8 +44,7 @@ class RandomCheck(unittest.TestCase):
         )
 
     def test_card_shuffle_arbitrary_cut(self):
-        card_order_len = len(card_order)
-        mixed_up = CardShuffle.shuffle_cards(card_order, list(range(card_order_len)))
+        mixed_up = CardShuffle.shuffle_cards(card_order, self.new_deck_order_positions)
         maybe_cut_up, _ = CardShuffle.maybe_cut(mixed_up, isArbitrary=True)
 
         jaro_similarity = CardShuffleStats.get_jaro_edit_distance_from(maybe_cut_up, card_order)
@@ -53,10 +54,23 @@ class RandomCheck(unittest.TestCase):
         )
 
     def test_shuffle_for_peapod(self):
-        card_order_len = len(card_order)
-        mixed_up = CardShuffle.shuffle_cards(card_order, list(range(card_order_len)))
+        mixed_up = CardShuffle.shuffle_cards(card_order, self.new_deck_order_positions)
 
         peapod_count = CardShuffleStats.count_peapods_from(mixed_up, card_order)
+
+        self.assertTrue( peapod_count[0] <= 4,
+            "There should be around 4 consecutive pairs for any shuffled deck"
+        )
+
+        self.assertTrue( peapod_count[0] < peapod_count[1],
+            "There should be less consecutive than non-consecutive pairs for any shuffled deck"
+        )
+
+    def test_shuffle_cut_for_peapod(self):
+        mixed_up = CardShuffle.shuffle_cards(card_order, self.new_deck_order_positions)
+        maybe_cut_up, _ = CardShuffle.maybe_cut(mixed_up)
+
+        peapod_count = CardShuffleStats.count_peapods_from(maybe_cut_up, card_order)
 
         self.assertTrue( peapod_count[0] <= 3,
             "There should be around 3 consecutive pairs for any shuffled deck"
@@ -66,4 +80,16 @@ class RandomCheck(unittest.TestCase):
             "There should be less consecutive than non-consecutive pairs for any shuffled deck"
         )
 
-    #def test_card_shuffle_long(self):
+    def test_shuffle_cut_arbitrary_for_peapod(self):
+        mixed_up = CardShuffle.shuffle_cards(card_order, self.new_deck_order_positions)
+        maybe_cut_up, _ = CardShuffle.maybe_cut(mixed_up, isArbitrary=True)
+
+        peapod_count = CardShuffleStats.count_peapods_from(maybe_cut_up, card_order)
+
+        self.assertTrue( peapod_count[0] <= 4,
+            "There should be around 3 consecutive pairs for any shuffled deck"
+        )
+
+        self.assertTrue( peapod_count[0] < peapod_count[1],
+            "There should be less consecutive than non-consecutive pairs for any shuffled deck"
+        )
